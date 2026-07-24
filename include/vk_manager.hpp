78624d73bdd6c05eba93a2d85f09557d6ca7e472
byeLihua <byelihua@qq.com>
2026-07-25 00:42:14 +0800

    feat: create pipeline

--- include/vk_manager.hpp
+++ include/vk_manager.hpp
@@ -12,17 +12,21 @@ class VkManager {
   const std::array<const char*, 3> _enableInstanceExtensionNames = {
       "VK_KHR_surface", "VK_KHR_win32_surface",
       "VK_KHR_get_surface_capabilities2"};
-  const std::array<const char*, 1> _enableDeviceExtensionNames = {
-      "VK_KHR_swapchain"};
+  const std::array<const char*, 2> _enableDeviceExtensionNames = {
+      "VK_KHR_swapchain", "VK_KHR_shader_draw_parameters"};
+  const vk::Format _imageFormat = vk::Format::eB8G8R8A8Srgb;
 
   vk::raii::Context _context;
   vk::raii::Instance _instance = nullptr;
+  vk::raii::PhysicalDevice _physicalDevice = nullptr;
   vk::raii::Device _device = nullptr;
   vk::raii::SurfaceKHR _surface = nullptr;
   vk::raii::SwapchainKHR _swapchain = nullptr;
+  vk::raii::Pipeline _pipeline = nullptr;
 
   void createInstance();
   void createDevice();
+  void createPipline();
 
  public:
   VkManager() = delete;
--- src/vk_manager.cpp
+++ src/vk_manager.cpp
@@ -1,9 +1,12 @@
 #include "vk_manager.hpp"
 
+#include <fstream>
+
 VkManager::VkManager(Window* window) {
   createInstance();
   _surface = window->createSurface(&_instance);
   createDevice();
+  createPipline();
 }
 
 VkManager::~VkManager() {}
@@ -19,7 +22,6 @@ void VkManager::createInstance() {
 
 void VkManager::createDevice() {
   vk::raii::PhysicalDevices physicalDevices(_instance);
-  vk::raii::PhysicalDevice physicalDevice = nullptr;
 
   // choose a physical device
   for (const vk::raii::PhysicalDevice& each : physicalDevices) {
@@ -27,7 +29,7 @@ void VkManager::createDevice() {
         each.getProperties2();
     if (vk::PhysicalDeviceType::eDiscreteGpu ==
         physicalDeviceProperties.properties.deviceType) {
-      physicalDevice = vk::raii::PhysicalDevice(std::move(each));
+      _physicalDevice = vk::raii::PhysicalDevice(std::move(each));
     }
   }
 
@@ -35,7 +37,7 @@ void VkManager::createDevice() {
   const float queuePriority = 0.5f;
   std::array<vk::DeviceQueueCreateInfo, 1> deviceQueueCreateInfos{};
   std::vector<vk::QueueFamilyProperties2> queueFamilyProperties =
-      physicalDevice.getQueueFamilyProperties2();
+      _physicalDevice.getQueueFamilyProperties2();
   for (uint16_t index = 0; index < queueFamilyProperties.size(); index++) {
     if (queueFamilyProperties[index].queueFamilyProperties.queueFlags &
         vk::QueueFlagBits::eGraphics) {
@@ -46,22 +48,22 @@ void VkManager::createDevice() {
     }
   }
 
+  vk::PhysicalDeviceVulkan13Features deviceFeature{.dynamicRendering =
+                                                       vk::True};
   auto deviceCreateInfo =
-      vk::DeviceCreateInfo{}
+      vk::DeviceCreateInfo{.pNext = &deviceFeature}
           .setQueueCreateInfos(deviceQueueCreateInfos)
           .setPEnabledExtensionNames(_enableDeviceExtensionNames);
-  _device = vk::raii::Device(physicalDevice, deviceCreateInfo);
+  _device = vk::raii::Device(_physicalDevice, deviceCreateInfo);
 
   // create swapchain
   vk::PhysicalDeviceSurfaceInfo2KHR surfaceInfo{.surface = _surface};
   auto surfaceCapabilities =
-      physicalDevice.getSurfaceCapabilities2KHR(surfaceInfo);
-  auto surfaceFormats = physicalDevice.getSurfaceFormats2KHR(surfaceInfo);
-  auto surfacePresentModes = physicalDevice.getSurfacePresentModesKHR(_surface);
+      _physicalDevice.getSurfaceCapabilities2KHR(surfaceInfo);
   vk::SwapchainCreateInfoKHR swapchainCreateInfo{
       .surface = _surface,
       .minImageCount = surfaceCapabilities.surfaceCapabilities.minImageCount,
-      .imageFormat = vk::Format::eB8G8R8A8Srgb,
+      .imageFormat = _imageFormat,
       .imageColorSpace = vk::ColorSpaceKHR::eSrgbNonlinear,
       .imageExtent = surfaceCapabilities.surfaceCapabilities.currentExtent,
       .imageArrayLayers = 1,
@@ -72,4 +74,101 @@ void VkManager::createDevice() {
       .presentMode = vk::PresentModeKHR::eFifo,
       .clipped = true};
   _swapchain = vk::raii::SwapchainKHR(_device, swapchainCreateInfo);
+}
+
+void VkManager::createPipline() {
+  auto shaderCodeFile = std::ifstream("../../shader/shader.spv",
+                                      std::ios::ate | std::ios::binary);
+  std::vector<char> shaderCodeBuffer(shaderCodeFile.tellg());
+  shaderCodeFile.seekg(0, std::ios::beg);
+  shaderCodeFile.read(shaderCodeBuffer.data(), shaderCodeBuffer.size());
+  shaderCodeFile.close();
+  vk::ShaderModuleCreateInfo shaderModuleCreateInfo{
+      .codeSize = shaderCodeBuffer.size() * sizeof(char),
+      .pCode = reinterpret_cast<uint32_t*>(shaderCodeBuffer.data())};
+  auto shaderModule = vk::raii::ShaderModule(_device, shaderModuleCreateInfo);
+
+  vk::PipelineShaderStageCreateInfo vertexStageCreateInfo{
+      .stage = vk::ShaderStageFlagBits::eVertex,
+      .module = shaderModule,
+      .pName = "vertexMain"};
+  vk::PipelineShaderStageCreateInfo fragmentStageCreateInfo{
+      .stage = vk::ShaderStageFlagBits::eFragment,
+      .module = shaderModule,
+      .pName = "fragmentMain"};
+  vk::PipelineShaderStageCreateInfo shaderStageCreateInfo[] = {
+      vertexStageCreateInfo, fragmentStageCreateInfo};
+
+  vk::PipelineVertexInputStateCreateInfo vertexInputStateCreateInfo{};
+
+  vk::PipelineInputAssemblyStateCreateInfo inputAssemblyStateCreateInfo{
+      .topology = vk::PrimitiveTopology::eTriangleList};
+
+  vk::PhysicalDeviceSurfaceInfo2KHR surfaceInfo{.surface = _surface};
+  auto surfaceCapabilities =
+      _physicalDevice.getSurfaceCapabilities2KHR(surfaceInfo);
+  vk::Viewport viewport{
+      .x = 0,
+      .y = 0,
+      .width =
+          (float)surfaceCapabilities.surfaceCapabilities.currentExtent.width,
+      .height =
+          (float)surfaceCapabilities.surfaceCapabilities.currentExtent.height,
+      .minDepth = 0,
+      .maxDepth = 1};
+  vk::Rect2D scissor{
+      .offset = vk::Offset2D{0, 0},
+      .extent = surfaceCapabilities.surfaceCapabilities.currentExtent};
+  auto viewportStateCreateInfo =
+      vk::PipelineViewportStateCreateInfo{}.setViewports(viewport).setScissors(
+          scissor);
+
+  vk::PipelineRasterizationStateCreateInfo rasterizationStateCreateInfo{
+      .depthClampEnable = vk::False,
+      .rasterizerDiscardEnable = vk::False,
+      .polygonMode = vk::PolygonMode::eFill,
+      .cullMode = vk::CullModeFlagBits::eBack,
+      .frontFace = vk::FrontFace::eClockwise,
+      .depthBiasClamp = vk::False,
+      .lineWidth = 1};
+
+  vk::PipelineMultisampleStateCreateInfo multisampleStateCreateInfo{
+      .rasterizationSamples = vk::SampleCountFlagBits::e1,
+      .sampleShadingEnable = vk::False};
+
+  vk::PipelineColorBlendAttachmentState colorBlendAttachmentState{
+      .blendEnable = vk::False,
+      .colorWriteMask =
+          vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG |
+          vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA};
+  auto ColorBlendStateCreateInfo =
+      vk::PipelineColorBlendStateCreateInfo{.logicOpEnable = vk::False,
+                                            .logicOp = vk::LogicOp::eCopy}
+          .setAttachments(colorBlendAttachmentState);
+
+  vk::PipelineLayoutCreateInfo layoutCreateInfo{.setLayoutCount = 0,
+                                                .pushConstantRangeCount = 0};
+  vk::raii::PipelineLayout pipelineLayout(_device, layoutCreateInfo);
+
+  auto graphicsPipelineCreateInfo =
+      vk::GraphicsPipelineCreateInfo{
+          .pVertexInputState = &vertexInputStateCreateInfo,
+          .pInputAssemblyState = &inputAssemblyStateCreateInfo,
+          .pViewportState = &viewportStateCreateInfo,
+          .pRasterizationState = &rasterizationStateCreateInfo,
+          .pMultisampleState = &multisampleStateCreateInfo,
+          .pColorBlendState = &ColorBlendStateCreateInfo,
+          .pDynamicState = nullptr,
+          .layout = pipelineLayout,
+          .renderPass = nullptr}
+          .setStages(shaderStageCreateInfo);
+  auto renderingCreateInfo =
+      vk::PipelineRenderingCreateInfo{}.setColorAttachmentFormats(_imageFormat);
+  vk::StructureChain<vk::GraphicsPipelineCreateInfo,
+                     vk::PipelineRenderingCreateInfo>
+      pipelineCreateInfoChain{graphicsPipelineCreateInfo, renderingCreateInfo};
+
+  _pipeline = vk::raii::Pipeline(
+      _device, nullptr,
+      pipelineCreateInfoChain.get<vk::GraphicsPipelineCreateInfo>());
 }
